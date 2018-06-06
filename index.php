4f120e856423594d2414e52427419f8be9a4576d
HavreEole <38245778+HavreEole@users.noreply.github.com>
2018-06-07 01:56:01 +0200

    Version Gandi - stats à jour

--- index.php
+++ index.php
@@ -6,21 +6,23 @@
 
 <!-- PRESENTATION Hébergeur ============================================================== -->
   <section><h2>Informations sur mon hébergeur</h2>
-           <p><strong>Nom : </strong> OVH</p>
-           <p><strong>Logo : </strong> <img src="img/ovh.jpg"/></p>
-           <p><strong>Siège social : </strong> 2 rue Kellermann - 59100 Roubaix - France</p>
+           <p><strong>Nom : </strong> Gandi</p>
+           <p><strong>Logo : </strong> <img src="img/gandi.jpg" /></p>
+           <p><strong>Siège social : </strong> 63, 65 Boulevard Massena 75013 Paris</p>
            <p><strong>Lien d'inscription : </strong>
-           <a href="https://www.ovh.com/fr/" target="_blanck" >Inscription OVH</a></p>
+           <a href="https://www.gandi.net/fr/simple-hosting" target="_blanck" >Inscription Gandi</a></p>
   </section>
   <section><h2>Informations sur la page question.php</h2>
-           <p><strong>Temps de chargement total en ms( https://tools.pingdom.com/ ): </strong> 649 ms</p>
-           <p><strong>Temps lié hébergeur (ms) : </strong> 1 200 s</p>
-           <p><strong>Temps lié image (ms) : </strong> 46 ms</p>
-           <p><strong>Temps lié CSS (ms) : </strong> 42 ms</p>
-           <p><strong>Temps lié JS (ms) : </strong> 10 ms</p>
+           <p><strong>Temps de chargement total en ms( https://tools.pingdom.com/ ): </strong> 295 ms</p>
+           <p><strong>Temps lié hébergeur (ms) : </strong> 187 s</p>
+           <p><strong>Temps lié image (ms) : </strong> 0 ms</p>
+           <!-- Il n'y a pas d'image sur question.php, mais sur index.php cela dure 172 ms. -->
+           <p><strong>Temps lié CSS (ms) : </strong> 104 ms</p>
+           <p><strong>Temps lié JS (ms) : </strong> 0 ms</p>
            <p><strong>Adresse IP d'hébergement de la page question.php 
-           (http://www.mon-ip.com/adresse-ip-site-internet.php) : </strong> 217.70.180.152</p>
-           <p><strong>Nom du serveur d'hébergement de la page question.php : </strong> gpaas12.dc0.gandi.net</p>
+           (http://www.ipvoid.com/find-website-ip/) : </strong> 155.133.138.10</p>
+           <p><strong>Nom du serveur d'hébergement de la page question.php : </strong> webacc8.sd5.ghst.net</p>
+           <!-- https://www.uptrends.fr/outils-gratuits/speed-test-site-web indique aussi "Server IP: 2001:4b98:dc5:253::10" -->
   </section>
   <section><h2>Complément d'information</h2>
            <p><strong>Une question complémentaire ? </strong> 
