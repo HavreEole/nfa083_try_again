5473f08acf0e443daa868240766ffc5a54bcea6b
HavreEole <38245778+HavreEole@users.noreply.github.com>
2018-06-07 00:41:44 +0200

    version OVH - stats pas à jour.

---
+++ inc/entete.inc.php
@@ -0,0 +1,19 @@
+<?php /* fichier cnam/nfa083/2017-cf1/inc/entete.inc.php - 20180604-PBO */
+
+  function entete($title,$actif) {
+    session_start(); ?>
+    <!DOCTYPE html><html lang="fr">
+    <head><title><?php echo $title; ?></title>
+          <meta charset='UTF-8' />
+          <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body><h1><?php echo $title; ?></h1>
+        <header>
+          <nav><a href="index.php" class="<?php if(!$actif){echo 'actif'; } ?>">
+                  Présentation hébergeur</a>
+               <a href="question.php" class="<?php if($actif){echo 'actif'; } ?>">
+                  Question Hébergement</a>
+          </nav>
+        </header>
+    <?php
+  } ?>
